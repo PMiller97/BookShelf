ce6633fb3d62a78128b899cf456e241233fe52cc
Paige Miller <paige_9753@hotmail.com>
2017-07-24 14:17:35 -0400

    added book and category models

---
+++ BookShelf/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BookShelf.Models
+{
+    public class Category
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<book> Books { get; set; } //ICollection is also a navigation property-is what, along with keys, allows me to connect my classes to eachother
+
+
+
+    }
+}
---
+++ BookShelf/Models/book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace BookShelf.Models
+{
+    public class book
+    {
+        [Key] //<-- primary key
+        public int ID { get; set; } //when you are building a model in C#, you ALWAYS want an ID
+        public string Title { get; set; }
+        public DateTime PublishedDate { get; set; }
+        public string Description { get; set; }
+        public string ISBN { get; set; }
+
+        [ForeignKey("Category")] //foreign key is how we establish a relationship with another class (or table)
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; } //when you see a line like this, it is called a navigation property
+
+    }
+}
